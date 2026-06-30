30469bad6a90e5f46210a9e3e8a5b87afb461613
Gouransh333 <gouransh333@gmail.com>
2026-06-30 10:20:48 +0530

    Remove sensitive credentials from arduino_secrets.h
    Cleared sensitive information from arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,3 +1,3 @@
-#define SECRET_DEVICE_KEY "fwev70nRoDT8dJBpHjM6kd53H"
-#define SECRET_OPTIONAL_PASS "4561pahwa"
-#define SECRET_SSID "jiohouse2.4"
+#define SECRET_DEVICE_KEY ""
+#define SECRET_OPTIONAL_PASS ""
+#define SECRET_SSID ""
